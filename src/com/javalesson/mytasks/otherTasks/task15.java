6492c553095b9141e02bac9b4d5eb7ee6b7b8ff4
Andygo298 <andygo298@gmail.com>
2019-11-18 17:44:43 +0300

    change task15-16

--- src/com/javalesson/mytasks/otherTasks/task15.java
+++ src/com/javalesson/mytasks/otherTasks/task15.java
@@ -1,4 +1,42 @@
-package com.javalesson.mytasks.otherTasks;
+/*
+Среди натуральных чисел от 1 до N(включая N), найдите число наибольшее по сумме цифр.
+Вывести это число и сумму его цифр.
 
+Входные данные
+В первой строке находится единственное натуральное число N, не превышающее 106.
+
+Выходные данные
+Выведите  само число, которое имеет максимальную сумму цифр и саму сумму цифр на одной строке через пробел.
+Если таких чисел несколько, выведите минимальное из них.
+
+Sample Input:
+
+20
+Sample Output:
+
+19 10
+ */
+
+package com.javalesson.mytasks.otherTasks;
+import java.util.Scanner;
 public class task15 {
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        int a = sc1.nextInt();
+        int x = 0;
+        int y = 0;
+        int z ;
+        int sum = 0;
+        for (int i = 1; i <= a; i++) {
+            for (z = i; z > 0; z /= 10) {
+                sum = sum + (z % 10);
+            }
+            if (y < sum) {
+                y = sum;
+                x = i;
+            }
+            sum = 0;
+        }
+        System.out.println(x + " " + y);
+    }
 }
--- src/com/javalesson/mytasks/otherTasks/task16.java
+++ src/com/javalesson/mytasks/otherTasks/task16.java
@@ -1,4 +1,37 @@
+/*
+Вывести представление целого числа N в виде произведения простых чисел.
+Входные данные
+В первой строке находится единственное натуральное число N, где 2≤N≤2147483647.
+Выходные данные
+Выведите список чисел в порядке не убывания через пробел.
+Sample Input 1:
+6
+Sample Output 1:
+2 3
+Sample Input 2:
+30
+Sample Output 2:
+2 3 5
+Sample Input 3:
+16
+Sample Output 3:
+2 2 2 2
+*/
+
 package com.javalesson.mytasks.otherTasks;
 
+import java.util.Scanner;
+
 public class task16 {
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        int n = sc1.nextInt();
+        for (int s = 2; s < n; s++) {
+            while (n % s == 0) {
+                System.out.print(s + " ");
+                n /= s;
+            }
+        }
+        if (n > 1) System.out.print(n);
+    }
 }
